86a5c4f2c9148171e24d6d5dd77bb64d04b9deb9
parthchoksi45 <68495663+parthchoksi45@users.noreply.github.com>
2020-11-13 07:44:36 +0530

    Update cscan.js

--- js/cscan.js
+++ js/cscan.js
@@ -1,15 +1,16 @@
 function cscanqueue()
 {
+    seek_queue=[];
     var head=parseInt(start_track);
     var r=[];
     var l=[];
     l.push(0);
     r.push(parseInt(max_track_size-1));
-    for (var i = 0; i < request_queue.length; i++) {
-        if (request_queue[i] < head)
-            l.push(request_queue[i]);
-        if (request_queue[i] > head)
-            r.push(request_queue[i]);
+    for (var i = 0; i < request_queue3.length; i++) {
+        if (request_queue3[i] < head)
+            l.push(request_queue3[i]);
+        if (request_queue3[i] > head)
+            r.push(request_queue3[i]);
     }    
     l.sort(function(a, b){return a - b});
     r.sort(function(a, b){return a - b});
@@ -35,4 +36,12 @@ function cscanqueue()
             seek_queue.push(temp);
         } 
     }
+    var z=start_track;
+var seek_time=0;
+for(var k=0;k<seek_queue.length;k++)
+{
+    seek_time+=Math.abs(seek_queue[k]-z);
+    z=seek_queue[k];
+}
+return seek_time;
 }
